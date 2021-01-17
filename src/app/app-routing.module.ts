7d92e7874d8f61f0f921f2cdbf738acf65f4b04b
Igor Xavier <igoorlimaa@gmail.com>
2021-01-17 20:27:27 -0300

    adiciona rotas de autenticaçao e logica do AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/account/login/login.component';
+import { SignupComponent } from './components/account/signup/signup.component';
+import { AuthenticationComponent } from './components/layouts/authentication/authentication.component';
 import { HomeComponent } from './components/layouts/home/home.component';
+import { AuthGuard } from './guards/shared/auth.guard';
 
 const routes: Routes = [
   {
@@ -8,6 +12,16 @@ const routes: Routes = [
     component: HomeComponent,
     children: [
       // all authentication based routes go here
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
     ]
   }
 ];
